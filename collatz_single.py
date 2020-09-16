b2fbe304f8363be85ceef9f1ac4ffc5cd21e435d
Erin McCurry <erin@Erins-MacBook-Air.local>
2020-09-15 22:19:33 -0400

    Completed part 1

--- collatz_single.py
+++ collatz_single.py
@@ -1,1 +1,14 @@
+def collatz(n):
+  counter=0
+
+  while n>1:
+    if (n % 2)== 1:
+      n=3*n + 1
+    else:
+      n=n/2
+    counter=counter+1
+    print(n)
+  print("C(n)=",counter)
+n = int(input('Enter n: '))
+collatz(n)
 
