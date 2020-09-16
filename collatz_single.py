0b9332938f04bcff61dcd0b09faf911aa6e08e99
Erin McCurry <erin@Erins-MacBook-Air.local>
2020-09-16 16:32:36 -0400

    added comments

--- collatz_single.py
+++ collatz_single.py
@@ -3,12 +3,16 @@ def collatz(n):
 
   while n>1:
     if (n % 2)== 1:
+      #n is odd
       n=3*n + 1
     else:
+      #n is even
       n=n/2
     counter=counter+1
+    #increases iteration counter by 1
     print(n)
   print("C(n)=",counter)
 n = int(input('Enter n: '))
 collatz(n)
+
 
